df25066a37ee5581dc45019a88c140c4735fe643
Plmxrs <kantapong00002@pcshsloei.ac.th>
2026-01-21 20:13:33 +0700

    fix: update port config for Render

--- backend/app.py
+++ backend/app.py
@@ -130,4 +130,5 @@ def _build_cors_preflight_response():
     return response
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=5000, debug=True)
